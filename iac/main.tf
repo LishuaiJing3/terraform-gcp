f97f72121d9c30c38897b0a06332261682a5490e
lishuai <lishuai.jing@gmail.com>
2024-07-11 21:59:54 +0200

    add cicd for terraform

---
+++ iac/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  credentials = "${file("${path.module}/account.json")}"
+  project     = "looker-evaluation"
+  region      = "europe-west1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "lj-demo-bucket-1"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
+}
